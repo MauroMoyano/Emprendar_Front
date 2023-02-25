0d8a3bf8d7d9913e5c0ecc72e7a101a5ec4a4452
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-02-25 17:49:46 -0300

    Subiendo repositorio

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import _app from './_app'
 
 const inter = Inter({ subsets: ['latin'] })
 
