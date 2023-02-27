963e1711e7ea8e6f845a7cd679f9d62475644477
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-02-27 19:36:54 -0300

    Store/Redux boilerplate

---
+++ redux/reducer.js
@@ -0,0 +1,25 @@
+// import {} from './actions'
+
+
+const initialState = {
+
+    allProjects: [],
+    allProjectsCopia: [],
+    detailUsuario: {},
+    userProjects: [],
+    dashAdmin: { project: [], users : []},
+
+
+}
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        /*case GET_HOME_CARDS:
+            const {home, auxDiets} = action.payload
+            return {...state, foods: home, diet: auxDiets, foodsCopy: home, currentPage: 0}*/
+
+        default:
+            return {...state}
+    }
+}
+export default rootReducer;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
 // import '@/styles/globals.css'
+import { Provider } from 'react-redux'
+import { useStore } from '../../redux/store'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const store = useStore(pageProps.initialReduxState)
+
+    return (
+        <Provider store={store}>
+            <Component {...pageProps} />
+        </Provider>
+    )
 }
- 
