c57bd180bcd092aa4568517d61d4b9c3838c874b
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-03-01 05:29:54 -0300

    segui la ruta de home hasta renderizar el detail

--- components/cardUser.jsx
+++ components/cardUser.jsx
@@ -1,11 +1,11 @@
 
 
 
-export default function cardUser(){
+export default function cardUser({userId}){
     return(
         <div>
             <h1>Este es el componente CardUser</h1>
-            
+            <h1>{userId}</h1>
         </div>
     )
 }
--- redux/actions.js
+++ redux/actions.js
@@ -1,13 +1,13 @@
 import axios from "axios"
-// require("dotenv").config()
+
 export const CURRENT_PAGE = "CURRENT_PAGE"
 export const GET_HOME_PROJECTS = "GET_HOME_PROJECTS"
 
-const BACK_APP_URL = process.env.BACK_APP_URL
+// const BACK_APP_URL = process.env.BACK_APP_URL
 
 export const getHomeProjects = () =>{
     return async function(dispatch) {
-        const {data} = await axios.get(BACK_APP_URL + "/project/")
+        const {data} = await axios.get("http://localhost:3001/project/")
         dispatch({type: GET_HOME_PROJECTS, payload: data})
     }
 }
