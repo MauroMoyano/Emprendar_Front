43c16845a6419261d63242043bebaad4fcc8eeea
Pedro Midueño <pedro.midueno@gmail.com>
2023-03-02 17:35:34 -0600

    Primeros estilos de home y Layout

--- components/CardProject.jsx
+++ components/CardProject.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useDispatch } from "react-redux";
-import style from "../src/pages/styles/cardProject.module.css"
+import style from "./styles/CardProject.module.css"
 import { getDetailProject, getUser } from "redux/actions";
 
 export default function cardProject(props) {
@@ -8,24 +8,29 @@ export default function cardProject(props) {
     //al tocar una tarjeta tendria que llenar el estado de detailProject de redux despachando la action  
     const dispatch = useDispatch()
 
-    const handlerDetailProject = (idProject) =>{
+    const handlerDetailProject = (idProject) => {
         dispatch(getDetailProject(idProject))
-        dispatch(getUser(props.userId)) 
-    } 
+        dispatch(getUser(props.userId))
+    }
 
 
 
     return (
         //link a ruta de detailuser paso params userId
-        <div onClick={()=>handlerDetailProject(props.idProject)} className={style.container}>
+        <div onClick={() => handlerDetailProject(props.idProject)} className={style.divGral}>
             <Link href={`/detailUser/${props.userId}`}>
-                <h1>{props.index}</h1>
-                <h1>{props.name}</h1>
-                <h1>{props.summary}</h1>
-                <h1>{props.description}</h1>
-                <h1>{props.date}</h1>
-                <h1>{props.goal}</h1>
-                <img src={props.img} alt="Image not found"/>
+                <div className={style.container}>
+                    <div className={style.divImg}>
+                        <img src={props.img} alt="Image not found" />
+                    </div>
+                    <div className={style.divInfo}>
+                        <h1>{props.name}</h1>
+                        <h4>{props.summary}</h4>
+                        <p className={style.description}>{props.description}</p>
+                        <p className={style.date}>{props.date}</p>
+                        <h3>Objetivo: ${props.goal}.00</h3>
+                    </div>
+                </div>
             </Link>
         </div>
     )
