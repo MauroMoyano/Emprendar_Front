f4006ce553da56363295b2708a35a44dbb5ee726
Nachito02 <arguellojuan08@gmail.com>
2023-03-13 20:06:10 -0300

    finalizando responsive detailProject

--- components/rutaDetail/CardSugerencias.jsx
+++ components/rutaDetail/CardSugerencias.jsx
@@ -79,7 +79,33 @@ export default function Sugerencia(props){
         slidesToShow: 3,
         slidesToScroll: 1,
         initialSlide: 0,
-
+        useCSS: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     };
 
 
@@ -90,7 +116,7 @@ export default function Sugerencia(props){
             <h3>PROYECTOS SIMILARES</h3>
             <div className={style.containerSug} >
                 <div >
-                    <Slider   arrowColor= "red"  className={style.scroll} {...settings} >
+                    <Slider className={style.scroll} {...settings} >
                     {
                         state
                         ? state?.map((obj)=>{
