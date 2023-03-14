8ffbedf252076252ed3612ad12a651c81faa35c4
Santiagoalvarez2022 <santiagoalvarez290401@gmail.com>
2023-03-14 18:47:55 -0300

    creacion de la vista del chat

---
+++ src/pages/chats.jsx
@@ -0,0 +1,21 @@
+import Layout from "components/Layout"
+
+function Message(props){
+    return(
+        <>hola</>
+    )
+}
+
+
+
+
+
+export default function Chats(props){
+
+    return(
+        <Layout>
+            <>hola</>
+
+        </Layout>
+    )
+}
