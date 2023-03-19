d339976e6e461967e6071b32af6ad17dcfccbd8f
arcanium <unicomparteargentina@gmail.com>
2023-03-19 15:00:25 -0300

    adaptacion de ReCharts - hacer NPM i

---
+++ components/statsCircle.jsx
@@ -0,0 +1,173 @@
+// import { PieChart, Pie, Cell, Legend } from "recharts";
+
+
+// export default function StatsCircle(props) {
+      
+//       const COLORS = ["#FFBB28", "#00C49F", "#FFBB28", "#FF8042"];
+      
+//       const RADIAN = Math.PI / 180;
+//       const renderCustomizedLabel = ({
+//         cx,
+//         cy,
+//         midAngle,
+//         innerRadius,
+//         outerRadius,
+//         percent,
+//         index
+//       }) => {
+//         const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+//         const x = cx + radius * Math.cos(-midAngle * RADIAN);
+//         const y = cy + radius * Math.sin(-midAngle * RADIAN);
+      
+//         return (
+//           <text
+//             x={x}
+//             y={y}
+//             fill="white"
+//             textAnchor={x > cx ? "start" : "end"}
+//             dominantBaseline="central"
+//           >
+//             {`${(percent * 100).toFixed(0)}%`}
+//           </text>
+//         );
+//       };
+
+
+
+//   return (
+//                 <PieChart id="donaron" width={300} height={250}>
+//                     <Pie
+//                         isAnimationActive={false}
+//                         data={props.data}
+//                         cx={150}
+//                         cy={100}
+//                         labelLine={false}
+//                         label={renderCustomizedLabel}
+//                         outerRadius={80}
+//                         fill="#8884d8"
+//                         dataKey="value"
+//                         nameKey="name"
+//                     >
+//                         {props.data.map((entry, index) => (
+//                         <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+//                         ))}
+//                     </Pie>
+//                     <Legend />
+//                 </PieChart>
+//   );
+// }
+
+
+
+
+import React, { useCallback, useState } from "react";
+import { PieChart, Pie, Cell, Sector } from "recharts";
+
+
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 5) * cos;
+  const sy = cy + (outerRadius + 5) * sin;
+  const mx = cx + (outerRadius + 15) * cos;
+  const my = cy + (outerRadius + 15) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 11;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+
+  return (
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 3}
+        outerRadius={outerRadius + 5}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333"
+      >{`${value}`}</text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999"
+      >
+        {`( ${(percent * 100).toFixed(0)}%)`}
+      </text>
+    </g>
+  );
+};
+
+export default function StatsCircle(props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onPieEnter = useCallback(
+    (_, index) => {
+      setActiveIndex(index);
+    },
+    [setActiveIndex]
+  );
+  const COLORS = ["#FFBB28", "#00C49F", "#FFBB28", "#FF8042"];
+
+  return (
+    <PieChart id="donaron" width={300} height={300}>
+      <Pie
+        isAnimationActive={false}
+        activeIndex={activeIndex}
+        activeShape={renderActiveShape}
+        data={props.data}
+        cx={150}
+        cy={150}
+        innerRadius={50}
+        outerRadius={70}
+        fill="#8884d8"
+        dataKey="value"
+        onMouseEnter={onPieEnter}
+      >
+      {
+        props.data.map((_, index)=> (
+          <Cell key={`cell-${index}`} fill={COLORS[index]} />
+        ))
+      }
+    </Pie>
+    </PieChart>
+  );
+}
--- src/pages/pruebaStats.jsx
+++ src/pages/pruebaStats.jsx
@@ -1,10 +1,32 @@
-import Stats from "../../components/stats.jsx";
+import StatsCircle from "../../components/statsCircle.jsx";
+import StatsRadar from "components/statsRadar.jsx";
+
+
+
 
 export default function pruebaStats (){
+
+    const data = [
+        {name: "Si donaron", value: 505},
+        {name: "No donaron", value: 105}
+    ]
+
+    const data2 = [
+        {name: "Tecnologia", value: 50},
+        {name: "Ambiental", value: 19},
+        {name: "Cultural", value: 36},
+        {name: "Social", value: 50},
+        {name: "Medicina", value: 55},
+        {name: "Educacion", value: 25},
+        {name: "Emprendimiento", value: 85},
+    ]
+
+
     return (
         <div>
             <h1>HolaMundo</h1>
-            <Stats />
+            <StatsCircle data={data} />
+            <StatsRadar data={data2} />
         </div>
     )
 }
