b87cbc63be4a208d474e79cf8ba1f9e3aa8b5fe6
Pedro Midueño <pedro.midueno@gmail.com>
2023-03-20 02:00:40 -0600

    commit para la daily 20/03/2023

--- components/profileProject.jsx
+++ components/profileProject.jsx
@@ -2,21 +2,37 @@ import ModalProject from "./ModalProject/ModalProject"
 import { useModal } from "./ModalProject/hooks/useModal"
 import modalStyle from './styles/modalProject.module.css'
 import style from './styles/profileProject.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
 
 
-export default function ProfileProject(props) {
+export default function ProfileProject({ projectData }) {
 
-    // console.log('props en profileProject ->', props)
+    console.log('projectData ->', projectData)
 
     const [isOpen, openModal, closeModal] = useModal(false)
 
     return (
-        <div>  
-            <div>Titulo: {props.projectData.title}</div>
-            <button onClick={openModal}>Abrir Modal</button>
+        <div>
+            <button onClick={openModal}><FontAwesomeIcon icon={faPenToSquare}/></button>
+            <div>
+                <img src={projectData.img} alt="" />
+            </div>
+            <h4>{projectData.title}</h4>
+            <h5>{projectData.summary}</h5>
             <ModalProject isOpen={isOpen} closeModal={closeModal}>
-                <h3 className={modalStyle.prueba}>Este es el modal</h3>
-                <h5 className={style.prueba2}>Titulo del proyecto: {props.projectData.title}</h5>
+                <div>
+                    {/* <img src={projectData.img} alt="" /> */}
+                    <h2 className={modalStyle.prueba}>{projectData.title}</h2>
+                    <h2 className={modalStyle.prueba}>{projectData.summary}</h2>
+                    <h2 className={modalStyle.prueba}>{projectData.description}</h2>
+                    <h2 className={modalStyle.prueba}>{projectData.goal}</h2>
+                    <h2 className={modalStyle.prueba}>{projectData.date}</h2>
+                    <h2 className={modalStyle.prueba}>{projectData.project_state}</h2>
+                    <h2 className={modalStyle.prueba}>{projectData.country.name}</h2>
+                    <h2 className={modalStyle.prueba}>{projectData.categories[0].name}</h2>
+                    
+                </div>
             </ModalProject>
         </div>
     )
