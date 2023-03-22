e401e78f1bae90f2a31afc0a9361a84efffde1ba
lechugaxthz <lautarogonzalogarcia@outlook.com>
2023-03-22 14:55:17 -0300

    timer del home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -33,6 +33,8 @@ export default function Home() {
     const [categoriess, setCategory] = useState('')
     const [search, setSearch] = useState('')
     const [searchs, setSearchs] = useState('')
+    const [timer, setTimer] = useState(null)
+
 
     useEffect(() => {
         let path
