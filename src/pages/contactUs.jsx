8ef4af70e9aaa2f48440fd0cb97c6b6fd0369572
Santiagoalvarez2022 <santiagoalvarez290401@gmail.com>
2023-03-22 19:38:13 -0300

    contact us ,mejoras

--- src/pages/contactUs.jsx
+++ src/pages/contactUs.jsx
@@ -20,6 +20,79 @@ export default function ContactUs() {
     const [alert,setAlert] = useState(null)
 
     let [input, setInput] = useState({
+        message: ""
+    })
+
+    const handleSumbit = async (e) => {
+        e.preventDefault()
+        try {
+            input.name  = User.user_name
+            input.email = User.email
+            const response = await clienteAxios.post('/user/contactUs/sendmessage',input)
+            setAlert(response.data.msg)
+
+            setInput({
+               
+                message: ""
+            })
+
+        } catch (error) {
+        }
+    }
+
+     
+    return (
+        <div >
+            <Head>
+                <title>Contacta con nosotros</title>
+            </Head>
+            <Layout>
+                <div className={style.fatherContainer}>
+                    <div className={style.container}>
+                        
+                        <img className={style.imgContainer} src="https://res.cloudinary.com/df4kwquv8/image/upload/v1679490079/emprendar_sources/qghgcdwxrxv4bepioxdv.jpg" alt="contacUsBanner" />
+                        
+                        <div className={style.barra}>
+                            <div className={style.backDiv}>
+                                <Link href={"/home"}>
+                                    <FontAwesomeIcon icon={faCircleArrowLeft} className={style.theIcon}/>
+                                </Link>
+                            </div>
+                            <div>
+                                <h2>CONTÁCTANOS</h2>
+                            </div>
+                        </div>
+
+                        <div className={style.card}>
+                            <div className={style.contact}> 
+                                <form onSubmit={handleSumbit} className={style.form}>
+                                    {alert && <p>{alert}</p>}
+                                        <input type="text" className={style.input} value={User?User.user_name:null}   readOnly placeholder='Tu nombre' />
+                                        <input type="text" className={style.input} placeholder='Tu mail' value={User?User.email:null}  readOnly />
+
+                                        <textarea name="" id="" value={input.message}  onChange={((e) => {setInput({...input,message:e.target.value})}) } className={style.textarea} placeholder="Mensaje" cols="30" rows="10"></textarea>
+
+                                        <button type="submit">Enviar</button>
+                                </form>          
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Layout>
+        </div>
+    )
+}
+
+/*export default function ContactUs() {
+
+    const User = useSelector(state => state.user )
+    useEffect(()=>{
+
+    }, [User])
+
+    const [alert,setAlert] = useState(null)
+
+    let [input, setInput] = useState({
         name: "",
         message: ""
     })
@@ -48,37 +121,29 @@ export default function ContactUs() {
             <Layout>
                 <div className={style.fatherContainer}>
                     <div className={style.container}>
-                        
-                        <img className={style.imgContainer} src="https://res.cloudinary.com/df4kwquv8/image/upload/v1679490079/emprendar_sources/qghgcdwxrxv4bepioxdv.jpg" alt="contacUsBanner" />
-                        
-                        <div className={style.barra}>
-                            <div className={style.backDiv}>
-                                <Link href={"/home"}>
-                                    <FontAwesomeIcon icon={faCircleArrowLeft} className={style.theIcon}/>
-                                </Link>
-                            </div>
-                            <div>
-                                <h2>CONTACTANOS</h2>
-                            </div>
+                        <div className={style.button}>
+                            <Link href="/home"><button className={style.buttonBack}><FontAwesomeIcon icon={faHouse} className={style.theIcon} /> HOME</button></Link>
                         </div>
 
                         <div className={style.card}>
-                            <div className={style.contact}> 
-                                <form onSubmit={handleSumbit} className={style.form}>
-                                    {alert && <p>{alert}</p>}
-                                        <input type="text" className={style.input} value={input.name} onChange={((e) => { setInput({ ...input, name: e.target.value }) })} placeholder='Tu nombre' />
-                                        <input type="text" className={style.input} placeholder='Tu mail' value={input.email} onChange={((e) => { setInput({ ...input, email: e.target.value }) })} />
+                        <div className={style.contact}>                  
+                            <h1>Contacta con nosotros</h1>
+                            <img src="https://cdn.pixabay.com/photo/2016/02/07/21/03/computer-1185626_1280.jpg" alt="" />
+                        </div>
 
-                                        <textarea name="" id="" value={input.message}  onChange={((e) => {setInput({...input,message:e.target.value})}) } className={style.textarea} placeholder="Mensaje" cols="30" rows="10"></textarea>
+                        <form onSubmit={handleSumbit} className={style.form}>
+                            {alert && <p>{alert}</p>}
+                                <input type="text" className={style.input} value={input.name} onChange={((e) => { setInput({ ...input, name: e.target.value }) })} placeholder='Tu nombre' />
+                                <input type="text" className={style.input} placeholder='Tu mail' value={User?User.email:null} readOnly  />
 
-                                        <button type="submit">Enviar</button>
-                                </form>          
-                            </div>
+                                <textarea name="" id="" value={input.message}  onChange={((e) => {setInput({...input,message:e.target.value})}) } className={style.textarea} placeholder="Mensaje" cols="30" rows="10"></textarea>
+
+                                <button type="submit">Enviar</button>
+                        </form>
                         </div>
                     </div>
                 </div>
+
             </Layout>
         </div>
-    )
-}
-
+    ) */
